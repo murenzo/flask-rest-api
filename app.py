0a8d87bc6e1534c97d00c8c2355607ed6a22da7a
Abdul <user@users-MacBook-Air.local>
2020-10-02 19:47:38 +0100

    Modify existing basic endpoints to flask restful conformed endpoint

--- app.py
+++ app.py
@@ -1,62 +1,13 @@
-from flask import Flask, jsonify, request
+from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
-
-stores = [
-    {
-        'name': 'My wonderful store',
-        'items': [
-            {
-                'name': 'My Item',
-                'price': 15.99
-            }
-        ]
-    }
-]
+api = Api(app)
 
 
-@app.route('/store', methods=['POST'])
-def create_store():
-    request_data = request.get_json()
-    new_store = {
-        'name': request_data['name'],
-        'items': []
-    }
-    stores.append(new_store)
-    return jsonify(new_store)
+class Student(Resource):
+    def get(self, name):
+        return {'student': name}
 
 
-@app.route('/store/<string:name>')
-def get_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify(store)
-
-    return jsonify({"message": "Store not found"})
-
-
-@app.route('/store')
-def get_stores():
-    return jsonify({'stores': stores})
-
-
-@app.route('/store/<string:name>/item', methods=['POST'])
-def create_store_item(name):
-    request_data = request.get_json()
-
-    for store in stores:
-        if store['name'] == name:
-            store['items'].append(
-                {'name': request_data['name'], 'price': request_data['price']})
-            return jsonify({"message": "Item created successfully"})
-
-    return jsonify({"message": "Store name not found, item could not be created"})
-
-
-@app.route('/store/<string:name>/item')
-def get_store_item(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify({"items": store['items']})
-
-    return jsonify({"message": "Store name not found"})
+api.add_resource(Student, '/student/<string:name>')
