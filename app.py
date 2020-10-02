8d409a826e3754bccf2549a541225f07e06701c3
Abdul <user@users-MacBook-Air.local>
2020-10-02 23:35:19 +0100

    Complete all other endpoints, implement basic security by filtering data inside incoming request

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -14,6 +14,13 @@ items = []
 
 
 class Item(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+                        type=float,
+                        required=True,
+                        help='Field cannot be empty.')
+
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda item: item['name'] == name, items), None)
@@ -24,10 +31,27 @@ class Item(Resource):
         if next(filter(lambda item: item['name'] == name, items), None):
             return {"message": f"An Item with name {name} already exist"}, 400
 
-        data = request.get_json()
+        data = Item.parser.parse_args()
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda item: item['name'] != name, items))
+        return {"message": "Item deleted successfully"}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+
+        item = next(filter(lambda item: item['name'] == name, items), None)
+
+        if item is None:
+            new_item = {'name': name, 'price': data['price']}
+            items.append(new_item)
+        else:
+            item.update(data)
+        return {"message": "Item updated successfully"}
 
 
 class ItemList(Resource):
